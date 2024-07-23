446c2b78c4845f9e0dd8a32262157c81d521fbd5
ellamariani <ella.mariani.135@gmail.com>
2024-07-23 16:13:05 -0400

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,30 +17,27 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>My Journey</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+              My interest sparked in high school when I took an engineering pathway, with this experience it sparked my interest in how things work and how technology can be used to solve problems. The hands on projects and collaborative environment in my high school engineering classes laid a solid foundation for my passion for the field. As I progressed, I started learning various programming languages and frameworks. Today, I am skilled in many frameworks and languages.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              <h3>Full Stack Development</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+              On the front end, I enjoy creating responsive and optimized websites that provide a seamless user experience. I have worked on projects like a Currency Converter Chrome Extension and other projects down below, where I utilized my skills in JavaScript, HTML, and CSS to bring my ideas to life.On the back end, I am fascinated by the complexities of server side logic and database management. I have experience developing fast and optimized back end systems and APIs.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <img src={getImageUrl("about/gaming.png")} alt="Gaming icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h3>Personal Interests</h3>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+                Outside of my academic and profession pursuits, I enjoy reading, gaming, working out, and experimenting with new baking recipes in the kitchen. I am always eager to learn new things and take on new challenges. 
               </p>
             </div>
           </li>
